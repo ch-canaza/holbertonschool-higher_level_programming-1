59f8515abecc4091b34ace145be4052a2fc27119
Lizzie Turner <hello@lizzieturner.com>
2018-04-10 22:05:01 +0000

    Change subtraction order

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if b < a:
+    if a < b:
         return (c)
 
     if b > c:
         return (a + b)
 
     num = a * b
-    return (c - num)
+    return (num - c)
