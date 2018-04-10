f11a2b55ab30ed70a31e9106d24d1b9a1afad45d
Lizzie Turner <hello@lizzieturner.com>
2018-04-10 19:14:29 +0000

    Eliminate unnecessary elif statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,9 +10,7 @@ else:
 
 if number % 10 == 0:
     print("and is 0")
-elif number < 0:
+elif number < 0 or number % 10 < 6:
     print("and is less than 6 and not 0")
-elif number % 10 > 5:
-    print("and is greater than 5")
 else:
-    print("and is less than 6 and not 0")
+    print("and is greater than 5")
