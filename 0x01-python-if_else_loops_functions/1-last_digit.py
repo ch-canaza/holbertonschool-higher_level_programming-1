e6115565aa0e1b82aeebed1f3d5f5ad218a4bbbf
Lizzie Turner <hello@lizzieturner.com>
2018-04-10 19:39:24 +0000

    Add digit variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is".format(number), end=" ")
 
-if number > 0 or number % 10 == 0:
-    print("{:d}".format(number % 10), end=" ")
+if number > 0:
+    digit = number % 10
 else:
-    print("-{:d}".format((number * -1) % 10), end=" ")
+    digit = ((number * -1) % 10) * -1
 
-if number % 10 == 0:
+print("Last digit of {:d} is {:d}".format(number, digit), end=" ")
+
+if digit == 0:
     print("and is 0")
-elif number < 0 or number % 10 < 6:
+elif number < 0 or digit < 6:
     print("and is less than 6 and not 0")
 else:
     print("and is greater than 5")
