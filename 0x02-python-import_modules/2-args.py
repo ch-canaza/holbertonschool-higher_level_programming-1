0663c8732f28d4ac37ee76aa6a73d2e397feb33a
Lizzie Turner <hello@lizzieturner.com>
2018-04-13 03:24:53 +0000

    Fix pep8 errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print("{}".format("0 arguments."))
     elif length == 1:
         print("{}".format("1 argument:"))
-        print("{}".format(argv[1]))
+        print("1: {}".format(argv[1]))
     else:
         print("{:d} {}".format(length, "arguments:"))
-        for i in range (1, length + 1):
+        for i in range(1, length + 1):
             print("{:d}: {}".format(i, argv[i]))
