35e119cab0ef14205e8159bc5f2f1ad6c4090c94
Lizzie Turner <hello@lizzieturner.com>
2018-04-17 23:20:41 +0000

    Fix empty list edge case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        max = 0
+        max = my_list[0]
         for num in my_list:
-            if num > max:
+            if num >= max:
                 max = num
         return max
     else:
