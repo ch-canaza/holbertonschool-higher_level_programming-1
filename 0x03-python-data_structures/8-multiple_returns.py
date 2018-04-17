c576cc107a51a12b84cbd8775cc6c7892960b49e
Lizzie Turner <hello@lizzieturner.com>
2018-04-17 23:11:13 +0000

    Create 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if sentence:
+        new = (len(sentence), sentence[0])
+    else:
         new = (0, None)
-        return new
-
-    new = (len(sentence), sentence[0])
-    return
+    return new
