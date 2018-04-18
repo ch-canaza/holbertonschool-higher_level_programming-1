e70c14100af57342d4389261365c057f073efb9d
Lizzie Turner <hello@lizzieturner.com>
2018-04-18 04:19:53 +0000

    Eliminate unnecessary if statements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        aa = 0
-        ab = 0
-    elif len(tuple_a) == 1:
-        aa = tuple_a[0]
-        ab = 0
-    else:
-        aa = tuple_a[0]
-        ab = tuple_a[1]
-    if len(tuple_b) == 0:
-        ba = 0
-        bb = 0
-    elif len(tuple_b) == 1:
-        ba = tuple_b[0]
-        bb = 0
-    else:
-        ba = tuple_b[0]
-        bb = tuple_b[1]
-    a = aa + ba
-    b = ab + bb
-    new_tuple = (a, b)
-    return new_tuple
+    a = tuple_a or (0,0)
+    b = tuple_b or (0,0)
+    if len(tuple_a) == 1:
+        a = (tuple_a[0], 0)
+    if len(tuple_b) == 1:
+        b = (tuple_b[0], 0)
+    new = (a[0] + b[0], a[1] + b[1])
+    return new
