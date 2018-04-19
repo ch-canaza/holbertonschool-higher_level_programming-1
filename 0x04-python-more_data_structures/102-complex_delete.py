f821c550747d8a176c8c2cefabf63fa8360aee7d
Lizzie Turner <hello@lizzieturner.com>
2018-04-19 23:42:03 +0000

    Create 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,4 @@ def complex_delete(a_dictionary, value):
         for k, v in list(a_dictionary.items()):
             if v == value:
                 del a_dictionary[k]
-        return a_dictionary
+    return a_dictionary
