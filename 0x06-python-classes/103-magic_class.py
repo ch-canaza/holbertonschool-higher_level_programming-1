c02964acb7f3729905c93d5ad11e809ea477c655
Lizzie Turner <hello@lizzieturner.com>
2018-05-02 05:38:08 +0000

    Make radius private

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -12,7 +12,7 @@ class MagicClass:
         initializes the circle
         """
         self.__radius = 0
-        if (type)radius is not int and type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
