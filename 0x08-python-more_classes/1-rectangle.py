13151a366efb6695fc4939ae9cde6320d94f7e1f
Lizzie Turner <hello@lizzieturner.com>
2018-05-08 21:32:51 +0000

    Account for missing arguments

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,9 +24,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ validates width as a positive integer """
-        if type(value) is not int:
+        if type(value) is not int or value == []:
             raise TypeError('width must be an integer')
-        if value < 0 or value == []:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -38,8 +38,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ validates height as a positive integer """
-        if type(value) is not int:
+        if type(value) is not int or value == []:
             raise TypeError('height must be an integer')
-        if value < 0 or value == []:
+        if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
