f2ed40c925e1fb7da3c0654e55ac22f3fd6abc09
Lizzie Turner <hello@lizzieturner.com>
2018-05-16 18:12:23 +0000

    Add f.seek() and eliminate unnecessary with

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,7 +5,7 @@ def read_lines(filename="", nb_lines=0):
     with open(filename) as f:
         for line in f:
             count += 1
-    with open(filename) as f:
+        f.seek(0)
         if 0 < nb_lines < count:
             for i in range(nb_lines):
                 print(f.readline(), end="")
