d1c9999c49b793c67a4d9b46fad395073df499e3
Lizzie Turner <hello@lizzieturner.com>
2018-05-21 22:57:42 +0000

    Create base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ this module contains the class Base """
+import json
+
 
 class Base:
     """ empty Base class """
@@ -8,7 +10,56 @@ class Base:
     def __init__(self, id=None):
         """ initializes Base """
         if id is None:
-            __nb_objects += 1
-            self.id = __nb.objects
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns json representation of list_dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns list of json string representation """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes json string represntation of list_objs """
+        new_list = []
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            for obj in list_objs:
+                new_list.append(obj.to_dictionary())
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(new_list))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns instance with attributes already set """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        cls.update(dummy, **dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ return list of instances """
+        new_list = []
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as f:
+                new_list = cls.from_json_string(f.read())
+            for i, j in enumerate(new_list):
+                new_list[i] = cls.create(**new_list[i])
+        except:
+            pass
+        return new_list
