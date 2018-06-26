3e940e1ade419edfce296a4c45fe2a1a5db1d6e4
Lizzie Turner <hello@lizzieturner.com>
2018-06-26 05:22:46 +0000

    Create 100-move_to_utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- converts hbtn_0c_0 to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+USE htbn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+
