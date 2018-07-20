9992533415a3f099539a6f356d57a146a2bf0c28
Lizzie Turner <hello@lizzieturner.com>
2018-07-20 00:42:58 +0000

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' lists all states from database hbtn_0e_0_usa'''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * from states ORDER BY states.id")
+    state_list = cur.fetchall()
+    for state in state_list:
+        print(state)
+    cur.close()
+    db.close()
