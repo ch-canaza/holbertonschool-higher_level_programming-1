30970d2490026f0890042e11dfd036f2130e3193
Lizzie Turner <hello@lizzieturner.com>
2018-07-20 01:41:42 +0000

    Create 5-filter_citites

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' lists all cities in a particular state '''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s ORDER BY cities.id",
+                (argv[4], ))
+    city_list = cur.fetchall()
+    first = 0
+    for city in city_list:
+        if first != 0:
+            print(", ", end="")
+        print("%s" % city, end="")
+        first += 1
+    print("")
+    cur.close()
+    db.close()
