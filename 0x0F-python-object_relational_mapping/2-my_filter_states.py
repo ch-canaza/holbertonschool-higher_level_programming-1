4fd136be6d3a47dda096cf8816d90af516e21ff1
Lizzie Turner <hello@lizzieturner.com>
2018-07-20 19:15:46 +0000

    Make query case-sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * from states WHERE name LIKE '{}' ORDER BY states.id"
+    cur.execute("SELECT * from states\
+                WHERE name LIKE '{}' COLLATE latin1_general_cs\
+                ORDER BY states.id"
                 .format(argv[4]))
     state_list = cur.fetchall()
     for state in state_list:
