95cb74e5430c2fb0cb01067875e257b6994e2792
Lizzie Turner <hello@lizzieturner.com>
2018-08-01 06:14:06 +0000

    Fix shellcheck errors

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request that causes serves to send "You got me!" as a response
-curl -so /dev/null/ -w "You got me!"  0.0.0.0:5000/catch_me
+curl -sLX PUT "You got me!" -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
