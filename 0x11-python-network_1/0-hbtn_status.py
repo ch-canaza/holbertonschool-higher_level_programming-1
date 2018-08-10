b1b3080f39d9922bbda0dcd41015f8096341894a
Lizzie Turner <hello@lizzieturner.com>
2018-08-10 17:16:04 +0000

    Fix tabstop issue

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request as req
 if __name__ == "__main__":
     with req.urlopen('https://intranet.hbtn.io/status') as res:
         html = res.read()
-        print('Body Response:')
-        print("    - type: {}".format(type(html)))
-        print('    - content: {}'.format(html))
-        print('    - utf8 content: {}'.format(html.decode('utf-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
