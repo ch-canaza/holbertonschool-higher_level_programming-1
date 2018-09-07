3f30ee3f2cb25ee766881f43d2a90942af7d9f7f
Lizzie Turner <hello@lizzieturner.com>
2018-09-07 18:39:51 +0000

    Create 8-script

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+// fetches and lists all movie titles using a url
+$.get('https://swapi.co/api/people/5/?format=json', function (data, status) {
+  if (status === 'success') {
+    for (let i in data.results) {
+      $('ul#list_movies').append('<li>' + data.results[i].title + '</li>');
+    }
+  }
+});
